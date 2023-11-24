8bfea552d8728892c6d5bc3425a8971dce812e8e
NaufalParamaRafif <profesoraiueo@gmail.com>
2023-11-24 21:47:56 +0700

    #6 - Colours & Fonts

--- lib/main.dart
+++ lib/main.dart
@@ -5,13 +5,24 @@ void main() => runApp(MaterialApp(
     appBar: AppBar(
       title: Text("Naufal keren"),
       centerTitle: true,
+      backgroundColor: Colors.red,
     ),
     body: Center(
-      child: Text("Center"),
+      child: Text(
+        "Center",
+        style: TextStyle(
+          fontSize: 20,
+          fontWeight: FontWeight.bold,
+          letterSpacing: 2,
+          color: Colors.red,
+          fontFamily: 'VinaSans',
+        ),
+      ),
     ),
     floatingActionButton: FloatingActionButton(
       onPressed: () {},
       child: Text("click"),
+      backgroundColor: Colors.red,
     ),
   ),
 ));
